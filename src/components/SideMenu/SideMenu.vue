1fcdc85622b75c0b65acd1824098d078e8925c19
Mandrican Andrei <adyta094@gmail.com>
2020-04-05 12:18:28 +0300

    created custom component for menu items

--- src/components/SideMenu/SideMenu.vue
+++ src/components/SideMenu/SideMenu.vue
@@ -1,22 +1,70 @@
 <template>
     <div class="side-menu">
-        side menu
+        <div class="admin">            
+        </div>
+
+        <div class="menu-items">
+            <customItemLink
+                :to="'/bikes'"
+                :imgIconName="'Bike.svg'"
+                :name="'Bikes'"
+                :downIcon="true"
+             />
+             <customItemLink
+                :to="'/clients'"
+                :imgIconName="'clients.svg'"
+                :name="'Clients'"
+                :downIcon="false"
+             />
+             <customItemLink
+                :to="'/settings'"
+                :imgIconName="'settings.svg'"
+                :name="'Settings'"
+                :downIcon="false"
+             />
+        </div>
     </div>
 </template>
 
 
 <script>
+import customItemLink from './customItemLink'
+
 export default {
+    components: {
+        customItemLink
+    }
     
 }
 </script>
 
 <style lang="scss">
+.img {
+    display: block;
+}
+
 $menu_width: 240px;
 .side-menu {
     background: #2761F0;
     min-width: $menu_width;
     color: #FFF;
     border-bottom-right-radius: 60px;
+
+    h5 {
+        margin: 0;
+        font-weight: 300;
+        letter-spacing: 1px;
+        font-size: 16px;
+    }
 }
+
+/* admin */
+.admin {
+    height: 100px;
+}
+
+.router-link-exact-active {
+    color: #FFF;
+}
+
 </style>
---
+++ src/components/SideMenu/customItemLink.vue
@@ -0,0 +1,45 @@
+<template>
+    <router-link :to="to" class="item bikes">
+        <div>
+            <div class="item-icon">
+                <img :src="require(`@/assets/menu_items/${imgIconName}`)" alt="icon">
+            </div>
+            <h5>{{ name }}</h5>
+        </div>
+
+        <i v-if="downIcon" class="el-icon-caret-bottom"></i>
+    </router-link>
+</template>
+
+
+<script>
+export default {
+    props: ['to', 'imgIconName', 'name', 'downIcon']
+}
+</script>
+
+<style lang="scss">
+.item {
+    display: flex;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+    justify-content: space-between;
+    text-decoration: none;
+    color: #ffffffa1;
+
+    margin-bottom: 40px;
+    i {
+        font-size: 12px;
+    }
+    div {
+        display: flex;
+        align-items: center;
+    }
+}
+.item-icon {
+    width: 0px;
+    margin-right: 35px;
+}
+
+</style>
